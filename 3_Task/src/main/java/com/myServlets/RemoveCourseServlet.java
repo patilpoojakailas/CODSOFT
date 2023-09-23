e256511092ed91acb267d07cbd4af5ce8076926b
patilpoojakailas <108522188+patilpoojakailas@users.noreply.github.com>
2023-09-23 16:41:09 +0530

    Update RemoveCourseServlet.java

--- 3_Task/src/main/java/com/myServlets/RemoveCourseServlet.java
+++ 3_Task/src/main/java/com/myServlets/RemoveCourseServlet.java
@@ -19,26 +19,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class RemoveCourseServlet
- */
 @WebServlet("/RemoveCourseServlet")
 public class RemoveCourseServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public RemoveCourseServlet() {
         super();
-        // TODO Auto-generated constructor stub
+      
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
@@ -46,37 +37,30 @@ public class RemoveCourseServlet extends HttpServlet {
             throws ServletException, IOException {
         String courseCode = request.getParameter("courseCode");
 
-        // Define your database connection details
         String jdbcUrl = "jdbc:mysql://localhost:3306/course";
 	    String dbUser = "root";
 	    String dbPassword = "7890";
 
         try {
-            // Load the JDBC driver
             Class.forName("com.mysql.cj.jdbc.Driver");
             
-            // Establish the database connection
+          
             Connection connection = DriverManager.getConnection(jdbcUrl, dbUser, dbPassword);
-            
-            // Define the SQL query to remove the course registration
+           
             String removeQuery = "DELETE FROM student WHERE course_code = ?";
             
-            // Create a PreparedStatement to execute the query
             PreparedStatement preparedStatement = connection.prepareStatement(removeQuery);
             preparedStatement.setString(1, courseCode);
             
-            // Execute the removal query
             preparedStatement.executeUpdate();
             
-            // Close the database resources
             preparedStatement.close();
             connection.close();
             
-            // Send a response to indicate successful removal
             response.getWriter().write("Course registration removed successfully.");
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
-            // Handle any exceptions (e.g., log the error or display an error message)
+         
             response.getWriter().write("Error: " + e.getMessage());
         }
     }
